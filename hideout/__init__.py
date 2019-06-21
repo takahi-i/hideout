5fd903605d50f5c52188b2a6a2fec6306594153e
takahi-i <takahi-i@cookpad.com>
2019-06-21 16:02:49 +0900

    Generate directory if not exist

--- hideout/__init__.py
+++ hideout/__init__.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
+import os
+
 from hideout.keeper import Keeper
 
 
 def keeper(force=False, skip_list=[], base_dir="./"):
+    if not os.path.exists(base_dir):
+        os.mkdir(base_dir)
     return Keeper(force, skip_list, base_dir)
