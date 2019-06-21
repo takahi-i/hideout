082f386676c8330f42cf814780b28e1105304b73
takahi-i <takahi-i@cookpad.com>
2019-06-21 14:14:57 +0900

    Succeeded to upload the package to pypi

--- README.rst
+++ README.rst
@@ -2,3 +2,18 @@
 hideout 
 =====================================================
 
+
+
+For Developers
+---------------
+
+
+Install to local environment
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$ python setup.py install
+
+Upload to pypi
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+$ python setup.py sdist upload
--- setup.py
+++ setup.py
@@ -4,15 +4,18 @@
 """The setup script."""
 import pip
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
-LONG_DESCRIPTION = """
-"""
+
+LONG_DESCRIPTION = """tiny cache file manager for data analysis"""
 
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = [ str(requirement.req) for requirement in parse_requirements('requirements.txt', session = pip.download.PipSession()) ]
+requirements = [ str(requirement.req) for requirement in parse_requirements('requirements.txt', session="production") ]
 
 setup_requirements = [
     # TODO(takahi-i): put setup requirements (distutils extensions, etc.) here
