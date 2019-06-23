8c93d1a8d46cfc36e3edbc0c422aa4259707858c
takahi-i <takahi-i@cookpad.com>
2019-06-23 14:47:18 +0900

    Bump v0.0.7

--- hideout/keeper.py
+++ hideout/keeper.py
@@ -5,17 +5,21 @@ from hideout import env
 
 
 class Keeper:
-    def __init__(self, force=False, skip_list=[], base_dir="./"):
-        self.base_dir = base_dir
-        self.skip_list = skip_list
+    def __init__(self, base_dir, force, skip_list=[]):
+        self.base_dir = base_dir or env.HIDEOUT_BASEDIR
         self.force = force or env.HIDEOUT_FORCE_CACHE
+        self.skip_list = skip_list
 
     def freeze(self, target, file_name, force=True):
-        if (not os.path.exists(file_name)) or (not force):
-            with open("{}/{}".format(env.HIDEOUT_BASEDIR, file_name), mode='wb') as f:
+        file_path="{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+        if (not os.path.exists(file_path)) or force:
+            with open(file_path, mode='wb') as f:
                 return pickle.dump(target, f)
+        return None
 
     def resume(self, file_name, force=False):
-        if os.path.exists(file_name) and not force:
-            with open("{}/{}".format(env.HIDEOUT_BASEDIR, file_name), mode='rb') as f:
+        file_path = "{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+        if os.path.exists(file_path) and not force:
+            with open(file_path, mode='rb') as f:
                 return pickle.load(file_name, f)
+        return None
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='hideout',
-    version='0.0.6',
+    version='0.0.7',
     description="",
     long_description=LONG_DESCRIPTION,
     author="Takahiko Ito",
