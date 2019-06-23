daa849ddc25b8bd717018040ee64c2cd5441703a
takahi-i <takahi-i@cookpad.com>
2019-06-23 14:51:26 +0900

    Bump v0.0.8

--- hideout/keeper.py
+++ hideout/keeper.py
@@ -11,14 +11,14 @@ class Keeper:
         self.skip_list = skip_list
 
     def freeze(self, target, file_name, force=True):
-        file_path="{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+        file_path="{}/{}".format(self.base_dir, file_name)
         if (not os.path.exists(file_path)) or force:
             with open(file_path, mode='wb') as f:
                 return pickle.dump(target, f)
         return None
 
     def resume(self, file_name, force=False):
-        file_path = "{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+        file_path = "{}/{}".format(self.base_dir, file_name)
         if os.path.exists(file_path) and not force:
             with open(file_path, mode='rb') as f:
                 return pickle.load(file_name, f)
