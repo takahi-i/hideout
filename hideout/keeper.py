e7005b4a4f4093a0c4259098567abfdfb45a1778
takahi-i <takahi-i@cookpad.com>
2019-06-25 00:01:42 +0900

    Bump v0.1.1

--- hideout/keeper.py
+++ hideout/keeper.py
@@ -1,23 +1,22 @@
 import os
 import pickle
+import contextlib
+from hideout import env
 
 
-class Keeper:
-    def __init__(self, base_dir, force, skip_list=[]):
-        self.base_dir = base_dir
-        self.force = force
-        self.skip_list = skip_list
+@contextlib.contextmanager
+def resume(file_name):
+    file_path = "{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+    if os.path.exists(file_path) and not env.HIDEOUT_FORCE_CACHE:
+        with open(file_path, mode='rb') as f:
+            yield pickle.load(file_name, f)
+    else:
+        yield None
 
-    def freeze(self, target, file_name, force=True):
-        file_path="{}/{}".format(self.base_dir, file_name)
-        if (not os.path.exists(file_path)) or force:
-            with open(file_path, mode='wb') as f:
-                return pickle.dump(target, f)
-        return None
 
-    def resume(self, file_name, force=False):
-        file_path = "{}/{}".format(self.base_dir, file_name)
-        if os.path.exists(file_path) and not force:
-            with open(file_path, mode='rb') as f:
-                return pickle.load(file_name, f)
-        return None
+@contextlib.contextmanager
+def freeze(target, file_name):
+    file_path="{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
+    if (not os.path.exists(file_path)) or env.HIDEOUT_FORCE_CACHE:
+        with open(file_path, mode='wb') as f:
+            return pickle.dump(target, f)
