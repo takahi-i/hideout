c3126ced9110b9df8a21fcfe8b8a2621c76231d0
takahi-i <takahi-i@cookpad.com>
2019-06-25 00:55:38 +0900

    Bump v0.1.5

--- hideout/__init__.py
+++ hideout/__init__.py
@@ -3,6 +3,7 @@ import os
 import pickle
 import contextlib
 from hideout import env, log
+from hideout.log import logger
 
 
 @contextlib.contextmanager
@@ -10,7 +11,7 @@ def resume(file_name):
     file_path = "{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
     target = None
     if os.path.exists(file_path) and not env.HIDEOUT_FORCE_CACHE:
-        log.info("found {}".format(file_path))
+        logger.error("found {}".format(file_path))
         with open(file_path, mode='rb') as f:
             target = pickle.load(f)
     yield target
@@ -21,6 +22,6 @@ def resume(file_name):
 def freeze(target, file_name):
     file_path="{}/{}".format(env.HIDEOUT_BASEDIR, file_name)
     if (not os.path.exists(file_path)) or env.HIDEOUT_FORCE_CACHE:
-        log.info("saving {}".format(file_path))
+        logger.info("saving {}".format(file_path))
         with open(file_path, mode='wb') as f:
             return pickle.dump(target, f)
