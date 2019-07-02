7956ab8fe1d44cfc042d3cb0639159efdaeda522
takahi-i <takahi-i@cookpad.com>
2019-07-02 13:06:41 +0900

    Bump v0.1.11

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='hideout',
-    version='0.1.10',
+    version='0.1.11',
     description="",
     long_description=LONG_DESCRIPTION,
     author="Takahiko Ito",
