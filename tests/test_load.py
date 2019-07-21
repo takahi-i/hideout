70e00bc800eeaa0f3cfa8ddb01d8b8ab12a14067
takahi-i <takahi-i@cookpad.com>
2019-07-21 23:05:30 +0900

    Fix lint

--- tests/test_load.py
+++ tests/test_load.py
@@ -13,7 +13,7 @@ class TestLoadCache(unittest.TestCase):
         env.HIDEOUT_BASEDIR = tempfile.mkdtemp()
 
     def test_resume_without_cache(self):
-        with hideout.resume(file_prefix="want-to-load-object") as want_to_load_object:
+        with hideout.resume("want-to-load-object") as want_to_load_object:
             if not want_to_load_object.succeeded_to_load():
                 want_to_load_object.set({"foobar": "baz"})
             self.assertEqual({"foobar": "baz"}, want_to_load_object.get())
@@ -22,12 +22,12 @@ class TestLoadCache(unittest.TestCase):
         org_object = {"foobar": "baz"}
         file_path = _generate_file_path("want-to-load-object")
         freeze(org_object, file_path)
-        with hideout.resume(file_prefix="want-to-load-object") as want_to_load_object:
+        with hideout.resume("want-to-load-object") as want_to_load_object:
             if not want_to_load_object.succeeded_to_load():
                 want_to_load_object.set({"foobar": "bar"})
             self.assertEqual(org_object, want_to_load_object.get())
 
     def test_resume_without_cache_without_set(self):
         with self.assertRaises(RuntimeError):
-            with hideout.resume(file_prefix="no-such-object") as want_to_load_object:
+            with hideout.resume("no-such-object") as want_to_load_object:
                 self.assertEqual(want_to_load_object.get(), None)
