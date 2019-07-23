722b609025c2a4c9bcb9d862480dda5ee2c515c2
takahi-i <takahi-i@cookpad.com>
2019-07-23 23:22:44 +0900

    Improve parsing bool

--- hideout/env.py
+++ hideout/env.py
@@ -1,6 +1,6 @@
 import os
-
+import distutils.util
 
 HIDEOUT_BASEDIR = os.getenv('HIDEOUT_BASEDIR', './')
 HIDEOUT_SKIP_STAGES = os.getenv('HIDEOUT_BASEDIR', '').split(",")
-HIDEOUT_SUPPRESS_CACHE = bool(os.getenv('HIDEOUT_SUPPRESS_CACHE', False))
+HIDEOUT_SUPPRESS_CACHE = bool(distutils.util.strtobool(os.getenv('HIDEOUT_SUPPRESS_CACHE', "False"))) # see https://qiita.com/koemu/items/fd333fd8ed14f31fbca6
