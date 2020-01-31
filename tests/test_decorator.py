08d925522694d373e82f6d34c30451015bb9e35f
takahi-i <takahikoito@gmail.com>
2020-01-31 10:38:40 +0900

    Enhance test cases

--- tests/test_decorator.py
+++ tests/test_decorator.py
@@ -12,15 +12,37 @@ def generate():
     return {"foobar": "bar"}
 
 
+class Generator:
+    @resume(label="generate")
+    def generate(self):
+        return {"foobar": "bar"}
+
 class TestLoadCache(unittest.TestCase):
 
     def setUp(self):
         env.HIDEOUT_CACHE_DIR = tempfile.mkdtemp()
         env.HIDEOUT_ENABLE_CACHE = True
 
+    def test_resume_without_cache(self):
+        want_to_load_object = generate()
+        self.assertEqual({"foobar": "bar"}, want_to_load_object)
+
     def test_resume_with_cache_decorator(self):
         org_object = {"foobar": "baz"}
         file_path = _generate_file_path_from_label("generate")
         freeze(org_object, file_path)
         want_to_load_object = generate()
+        self.assertEqual(org_object, want_to_load_object)
+
+    def test_resume_without_cache_from_instance(self):
+        generator = Generator()
+        want_to_load_object = generator.generate()
+        self.assertEqual({"foobar": "bar"}, want_to_load_object)
+
+    def test_resume_with_cache_method_decorator(self):
+        org_object = {"foobar": "baz"}
+        file_path = _generate_file_path_from_label("generate")
+        freeze(org_object, file_path)
+        generator = Generator()
+        want_to_load_object = generator.generate()
         self.assertEqual(org_object, want_to_load_object)
